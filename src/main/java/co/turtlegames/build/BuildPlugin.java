aaf34e175941c97a8cf75e83c3a20eead7449f71
Cameron <ramsellcameron@gmail.com>
2020-03-27 13:08:05 +0000

    initial code for build plugin

---
+++ src/main/java/co/turtlegames/build/BuildPlugin.java
@@ -0,0 +1,13 @@
+package co.turtlegames.build;
+
+import co.turtlegames.core.TurtlePlugin;
+
+public class BuildPlugin extends TurtlePlugin {
+
+    public BuildPlugin() {
+        super("Build");
+    }
+
+
+
+}
